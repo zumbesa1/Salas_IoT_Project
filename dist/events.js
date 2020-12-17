b4f4b6b07b3d58cb0be9b9e3e7fdf1db6bb7b830
Salahudin Zumberi <zumbesa1@students.zhaw.ch>
2020-12-17 17:44:02 +0100

    ad Charts and style

--- dist/events.js
+++ dist/events.js
@@ -1,15 +1,28 @@
-
 var app = new Vue({
     el: "#app",
     data: {
         messages: [],
         lastMessage: "",
         eventName: "",
-        arrayGasValues: [],
-        arrayTempValues: []
+        arrayGasValues: [
+          "60"
+        ],
+        lastTimeGas: "",
+        arrayTimestampGas: [
+          new Date().getTime()
+        ],
+        arrayTempValues: [
+          "21.00"
+        ],        
+        lastTimeTemp: "",
+        arrayTimestampTemp: [
+          new Date().getTime()
+        ],
     },
     mounted: function () {
         this.initSse();
+        this.createChartGas();
+        this.createChartTemp();
     },
     methods: {
         initSse: function () {
@@ -22,24 +35,106 @@ var app = new Vue({
                     this.eventName = JSON.parse(event.data).eventName;
                     var value = JSON.parse(event.data).value;
                     var message = JSON.parse(event.data).message;
-                    console.log(this.eventName + ": " + value);  
-                    
+                    var time = new Date().getTime();
+
                     if(this.eventName === "temperature"){
+                        this.lastTimeTemp = time;
+                        this.arrayTimestampTemp.push(this.lastTimeTemp);
                         this.arrayTempValues.push(value);
+                        this.createChartTemp();
+                        console.log(this.arrayTempValues.length);
+                        if(this.arrayTempValues.length > 30){
+                          this.arrayTempValues.splice(0,1);
+                          this.arrayTimestampTemp.splice(0,1);
+                        }
                     }
                     else if(this.eventName === "gasValue"){
+                        this.lastTimeGas = time;
+                        this.arrayTimestampGas.push(this.lastTimeGas)
                         this.arrayGasValues.push(value);
+                        this.createChartGas();
+                        console.log(this.arrayGasValues.length);
+                        if(this.arrayGasValues.length > 30){
+                          this.arrayGasValues.splice(0,1);
+                          this.arrayTimestampGas.splice(0,1);
+                        }
                     }
                     else{
                         message = "There is no data available";
                         console.log(message)
                     }
-                    console.log(this.eventName);
-
+                    
                 };
             } else {
                 this.message = "Your browser does not support server-sent events.";
             }
-        }
+        },
+        createChartGas: function() {
+            'use strict'
+  
+            feather.replace();          
+            var ctx = document.getElementById('myChartGas');
+              var myChart = new Chart(ctx, {
+                type: 'line',
+                data: {
+                  datasets: [{
+                    data: this.arrayGasValues,
+                    lineTension: 0,
+                    backgroundColor: 'lightgrey',
+                    borderColor: '#007bff',
+                    borderWidth: 1,
+                    pointBackgroundColor: '#007bff',
+                    pointRadius: 2
+                  }],
+                  labels: this.arrayTimestampGas,
+                },
+                options: {
+                  scales: {
+                    yAxes: [{
+                      ticks: {
+                        beginAtZero: false
+                      }
+                    }]
+                  },
+                  legend: {
+                    display: false
+                  }
+                }
+              })
+        },
+        createChartTemp: function() {
+            'use strict'
+  
+            feather.replace();     
+            var ctx = document.getElementById('myChartTemp');
+              var myChart = new Chart(ctx, {
+                type: 'line',
+                data: {
+                  datasets: [{
+                    label: 'Temp (°C)',
+                    data: this.arrayTempValues,
+                    lineTension: 0,
+                    backgroundColor: 'lightpink',
+                    borderColor: '#007bff',
+                    borderWidth: 1,
+                    pointBackgroundColor: '#007bff',
+                    pointRadius: 2
+                  }],
+                  labels: this.arrayTimestampTemp,
+                },
+                options: {
+                  scales: {
+                    yAxes: [{
+                      ticks: {
+                        beginAtZero: false
+                      }
+                    }]
+                  },
+                  legend: {
+                    display: false
+                  }
+                }
+              })
+        },
     }
 })
--- eventListeners.js
+++ eventListeners.js
@@ -40,7 +40,7 @@ function getTemperature (event) {
     // helper variables that we need to build the message to be sent to the clients
     let message = "Message angekommen";
     var floatValue = parseFloat(evData);
-    var correctedTempValue = floatValue - 4;
+    var correctedTempValue = ((floatValue - 4)*100)/100;
     let newTempValue = correctedTempValue.toString();
 
     // send data to all connected clients
